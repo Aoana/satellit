de5ded4eae2d9e0e628d52ea41c9a50d7fc4f934
Anders <andersmarkendahl@gmail.com>
2017-03-12 19:33:54 +0100

    src: update main with printouts

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
-test
+/* add.c
+ * a simple C program
+ */
+  
+#include <stdio.h>
+#define LAST 10
+  
+int main()
+{
+	int i, sum = 0;
+
+	for ( i = 1; i <= LAST; i++ ) {
+		sum += i;
+	}
+	printf("sum = %d\n", sum);
+	return 0;
+}
