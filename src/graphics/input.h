ab205bb9cdbce8dae97d52fc7af2d140c2587445
Anders <andersmarkendahl@gmail.com>
2017-04-29 20:46:50 +0200

    doc: documented input.h

--- src/graphics/input.h
+++ src/graphics/input.h
@@ -1,11 +1,30 @@
+/**
+ * @file
+ * @brief Input handling in game
+ */
 #ifndef INPUT_H
 #define INPUT_H
 #include "common.h"
 #include "gholder.h"
 #endif
 
-#define INPUT_INCR 5
+#define INPUT_INCR 5 /**< Velocity increment for input. */
 
 struct gholder;
-void input_get_intro(struct gholder *);
-void input_get_runtime(struct gholder *);
+
+/**
+ * @brief Get input during intro phase.
+ * @param gh Pointer to global game holder.
+ *
+ * Input handling from arrow keys for setting initial velocity.
+ * Handling of shutdown (ESC or closing screen).
+ */
+void input_get_intro(struct gholder *gh);
+
+/**
+ * @brief Get input during game phase.
+ * @param gh Pointer to global game holder.
+ *
+ * Handling of shutdown (ESC or closing screen).
+ */
+void input_get_runtime(struct gholder *gh);
