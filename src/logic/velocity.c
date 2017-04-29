f2abec786cee796abc33d94df840f0c403a4ca16
Anders <andersmarkendahl@gmail.com>
2017-04-29 21:19:42 +0200

    doc: Documented velocity

--- src/logic/velocity.c
+++ src/logic/velocity.c
@@ -1,28 +1,28 @@
 #include "velocity.h"
 
 /* Check velocity */
-int velocity_validate(double vx, double vy) {
-	return 0;
+enum velocityReturnCode velocity_validate(double vx, double vy) {
+	return VELOCITY_OK;
 }
 
-int velocity_update(object_list *ptl, object *pn) {
+enum velocityReturnCode velocity_update(object_list *ptl, object *rt) {
 
 	double a_x = 0, a_y = 0;
 	double f_x = 0, f_y = 0;
 	double dx, dy, d;
 	object *pt;
-	position *p = pn->pos;
+	position *p = rt->pos;
 
 	DL_FOREACH(ptl->head, pt) {
 		dx = pt->pos->x - p->x;
 		dy = pt->pos->y - p->y;
 		d = sqrt(pow(dx,2)+pow(dy,2));
-		f_x = f_x + (GRAV_CONST*pt->mass*pn->mass*dx)/pow(d,3);
-		f_y = f_y + (GRAV_CONST*pt->mass*pn->mass*dy)/pow(d,3);
+		f_x = f_x + (GRAV_CONST*pt->mass*rt->mass*dx)/pow(d,3);
+		f_y = f_y + (GRAV_CONST*pt->mass*rt->mass*dy)/pow(d,3);
 		//printf("DEBUG: d=[%f,%f,%f] f=[%f,%f,%f] p=[%f,%f] pt=[%f,%f]\n", dx, dy, d, f_x, f_y, f, p->x, p->y, pt->pos->x, pt->pos->y);
 	}
-	a_x = f_x/pn->mass;	
-	a_y = f_y/pn->mass;
+	a_x = f_x/rt->mass;
+	a_y = f_y/rt->mass;
 	
 	p->vx = p->vx + a_x/UPDATE_FREQ;
 	p->vy = p->vy + a_y/UPDATE_FREQ;
--- src/logic/velocity.h
+++ src/logic/velocity.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief Velocity handling
+ */
+
 #ifndef VELOCITY_H
 #define VELOCITY_H
 #include "common.h"
@@ -6,8 +11,12 @@
 #include "planet.h"
 
 
-#define GRAV_CONST 1000
+#define GRAV_CONST 1000 /**< Gravitational constant, only for tweaking */
 
+/**
+ * @brief Return codes for velocity handling.
+ *
+ */
 enum velocityReturnCode {
     VELOCITY_OK = 0,
     VELOCITY_NOK,
@@ -16,6 +25,21 @@ enum velocityReturnCode {
 struct object;
 struct object_list;
 
-int velocity_validate(double, double);
-int velocity_update(struct object_list *, struct object *);
+/**
+ * @brief Validate velocity vectors.
+ * @param vx Velocity in X.
+ * @param vy Velocity in Y.
+ * @return VELOCITY_OK if pass, VELOCITY_NOK if failed.
+ *
+ */
+enum velocityReturnCode velocity_validate(double vx, double vy);
+
+/**
+ * @brief Update velocity based on gravitation.
+ * @param ptl Pointer to object list that excerts gravitation (planet list).
+ * @param rt Pointer to object to update velocity.
+ * @return VELOCITY_OK if pass, VELOCITY_NOK if failed.
+ *
+ */
+enum velocityReturnCode velocity_update(struct object_list *ptl, struct object *rt);
 #endif
