0390dcf7fe76bd493596ad41a9fdb0a5dc1498c8
Anders <andersmarkendahl@gmail.com>
2017-04-30 11:10:19 +0200

    doc: documented position.h

--- src/logic/position.c
+++ src/logic/position.c
@@ -1,14 +1,5 @@
 #include "position.h"
 
-
-/* Check current resolution */
-int position_validate(double x, double y) {
-	if ((x >= SPACE_W_MIN && x <= SPACE_W_MAX) && (y >= SPACE_H_MIN && y <= SPACE_H_MAX)) {
-		return 0;
-	}
-	return 1;
-}
-
 position * position_init(double x, double y, double vx, double vy) {
 	position *p;
 	if (position_validate(x, y) != 0) {
@@ -27,8 +18,15 @@ int position_destroy(position *p) {
 	return 0;
 }
  
-int position_update(object_list *ptl, object *pn) {
-	position *p = pn->pos;
+enum positionReturnCode position_validate(double x, double y) {
+	if ((x >= SPACE_W_MIN && x <= SPACE_W_MAX) && (y >= SPACE_H_MIN && y <= SPACE_H_MAX)) {
+		return POSITION_OK;
+	}
+	return POSITION_ERR_OOB;
+}
+
+enum positionReturnCode position_update(object_list *objl, object *obj) {
+	position *p = obj->pos;
 	p->x = p->x + p->vx/UPDATE_FREQ;
 	p->y = p->y + p->vy/UPDATE_FREQ;
 	if (position_validate(p->x, p->y) != 0) {
@@ -36,7 +34,7 @@ int position_update(object_list *ptl, object *pn) {
 	}
 
 	/* Update velocity for next timestep */
-	velocity_update(ptl, pn);
+	velocity_update(objl, obj);
 	if (velocity_validate(p->vx, p->vy) != 0) {
 		return POSITION_ERR_VEL;
 	}
--- src/logic/position.h
+++ src/logic/position.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief Position handling
+ */
+
 #ifndef POSITION_H
 #define POSITION_H
 #include "common.h"
@@ -14,14 +19,50 @@ typedef struct position {
 	double vy;	/**< Velocity in Y. */
 } position;
 
+/**
+ * @brief Return codes for position handling.
+ */
 enum positionReturnCode {
     POSITION_OK = 0,	/**< Position OK. */
     POSITION_ERR_OOB,	/**< Position out of bounds. */
     POSITION_ERR_VEL,	/**< Faulty Velocity. */
 };
 
-int position_validate(double x, double y);
-position * position_init(double, double, double, double);
-int position_destroy(position *);
-int position_update(struct object_list *ptl, struct object *pn);
+/**
+ * @brief Initalize and allocate position.
+ * @param x Initial position in X.
+ * @param y Initial position in Y.
+ * @param vx Initial velocity in X.
+ * @param vy Initial velocity in Y.
+ * @return Pointer to created position, NULL if failed.
+ *
+ */
+position * position_init(double x, double y, double vx, double vy);
+
+/**
+ * @brief Destroy and free position.
+ * @param p Pointer to position to destroy.
+ * @return 0 if passed, non-zero if failed.
+ *
+ */
+int position_destroy(position *p);
+
+/**
+ * @brief Validate position towards boundaries of game.
+ * @param x Position in X.
+ * @param y Position in Y.
+ * @return POSITION_OK if pass, POSITION_ERR_OOB if out if bounds.
+ *
+ */
+enum positionReturnCode position_validate(double x, double y);
+
+/**
+ * @brief Update position and velocity.
+ * @param objl Pointer to object list to interact with.
+ * @param obj Pointer to object to be updated.
+ * @return POSITION_OK if pass, positionReturnCode if failed.
+ * @see positionReturncode
+ * @see velocity_update
+ */
+enum positionReturnCode position_update(struct object_list *objl, struct object *obj);
 #endif
