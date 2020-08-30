b5307240540112098b4f9b39bbf9ef780c91549e
Anders Markendahl <andersmarkendahl@gmail.com>
2020-08-30 12:31:44 +0200

    utils: cleanup of git-tagging script

--- util/git-tagging.sh
+++ util/git-tagging.sh
@@ -8,16 +8,16 @@ die() {
 
 test -n "$SAT_GIT" || die 'Not set [$SAT_GIT]'
 
-base=`cat $SAT_GIT/VERSION_PREFIX`
+base=$(cat $SAT_GIT/VERSION_PREFIX) || die "Unable to get base version"
 git tag | grep $base
 if test $?;then
-	oldbuild=`git tag | grep $base | tail -n 1 | cut -d '-' -f2`
+	oldbuild=$(git tag | grep $base | tail -n 1 | cut -d '-' -f2)
 	build=$((oldbuild+1))
 else
 	build=0
 fi
 
 t=$base-$build
-git tag $t
-git push origin $t
+git tag $t || die "Unable to tag $t locally"
+git push origin $t || die "Unable to push tag $t to origin"
 
